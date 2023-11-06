cecf0f656857c5c15cf30d14f7b4787b01a5c362
Anurag <dev.anuragkr@gmail.com>
2023-11-07 00:09:18 +0530

    created search component

---
+++ src/ui/SearchBox.tsx
@@ -0,0 +1,12 @@
+function SearchBox() {
+  return (
+    <form>
+      <input
+        placeholder='Send a message'
+        className='w-full rounded-xl px-2 py-3 shadow-lg bg-[#494c5ecf] outline-none '
+      />
+    </form>
+  );
+}
+
+export default SearchBox;
