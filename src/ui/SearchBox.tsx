85400598a9c70a9d68683368350ae1d7208efa8d
Anurag <dev.anuragkr@gmail.com>
2023-11-08 21:39:36 +0530

    updated search box

--- src/ui/SearchBox.tsx
+++ src/ui/SearchBox.tsx
@@ -1,9 +1,34 @@
+'use client';
+
+import { sendMessage } from '@/lib/action';
+import { useLocalStorage } from '@/lib/useLocalStorage';
+import { useEffect, useRef } from 'react';
+import { useFormState } from 'react-dom';
+
 function SearchBox() {
+  const ref = useRef<HTMLFormElement>(null);
+  const [value, setValue] = useLocalStorage('messages');
+
+  const [state, dispatch] = useFormState(sendMessage, {});
+
+  useEffect(() => {
+    if (state.response) setValue(state.response);
+  }, [setValue, state]);
+
   return (
-    <form>
+    <form
+      ref={ref}
+      action={async (formData: FormData) => {
+        // set message into local storage
+        setValue({ role: 'user', message: formData.get('message') });
+        ref.current?.reset();
+        await dispatch(formData);
+      }}
+    >
       <input
         placeholder='Send a message'
-        className='w-full rounded-xl px-2 py-3 shadow-lg bg-[#494c5ecf] outline-none '
+        name='message'
+        className='w-full resize-none rounded-xl px-3 py-4 shadow-lg bg-[#494c5ecf] outline-none'
       />
     </form>
   );
