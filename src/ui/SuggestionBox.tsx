511d19e1769ad2e57597e27c24dd807ad42c9beb
Anurag <dev.anuragkr@gmail.com>
2023-11-08 21:39:49 +0530

    updated suggestion box

--- src/ui/SuggestionBox.tsx
+++ src/ui/SuggestionBox.tsx
@@ -21,7 +21,7 @@ const data = [
 
 function SuggestionBox() {
   return (
-    <div className='grid grid-cols-1 grid-rows-4 gap-2'>
+    <div className='grid grid-cols-1 grid-rows-4 gap-2 md:grid-cols-2 md:grid-rows-2'>
       {data.map(({ title, content }) => (
         <SuggestionCard key={title} title={title} content={content} />
       ))}
