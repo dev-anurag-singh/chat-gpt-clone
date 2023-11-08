f371d9fb5c33645a0507209ac45ec3dafce3805c
Anurag <dev.anuragkr@gmail.com>
2023-11-08 21:38:28 +0530

    updated home page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,22 +1,13 @@
-import SearchBox from '@/ui/SearchBox';
-import SuggestionBox from '@/ui/SuggestionBox';
+import SearchAndSuggestion from '@/ui/SearchAndSuggestion';
 
 function HomePage() {
   return (
-    <div className='relative p-3 h-screen flex flex-col justify-center items-center'>
-      <div className='pb-60'>
-        <h1 className='text-4xl text-gray-600 font-semibold'>ChatGPT</h1>
-      </div>
-      <div className='absolute w-full bottom-0 right-0'>
-        <div className='p-3'>
-          <SuggestionBox />
+    <div className='max-w-4xl m-auto md:px-4'>
+      <div className='relative p-3 h-screen flex flex-col justify-center items-center'>
+        <div className='pb-60'>
+          <h1 className='text-4xl text-gray-600 font-semibold'>ChatGPT</h1>
         </div>
-        <div className='border-t border-gray-500 p-3 text-center'>
-          <SearchBox />
-          <span className='text-xs text-gray-300 '>
-            ChatGPT can make mistakes. Consider checking important information.
-          </span>
-        </div>
+        <SearchAndSuggestion />
       </div>
     </div>
   );
