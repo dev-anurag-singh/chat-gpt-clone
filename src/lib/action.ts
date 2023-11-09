a0bc98afcbd53bbe5bb5541e46ca845843882607
Anurag <dev.anuragkr@gmail.com>
2023-11-09 20:36:16 +0530

    added new api key and made some changes

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -8,14 +8,15 @@ const openai = new OpenAI({
 
 export async function sendMessage(prevState: any, formData: FormData) {
   const message = formData.get('message');
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: 'user', content: `${message}` }],
-    model: 'gpt-3.5-turbo',
-  });
+  try {
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: `${message}` }],
+      model: 'gpt-3.5-turbo',
+    });
 
-  console.log(completion.choices[0]);
-
-  return {
-    response: { role: 'assistant', message: 'Hello from the assistant' },
-  };
+    return completion.choices[0];
+  } catch (err) {
+    console.log(err);
+    return { message: 'Failed to get a response from openai' };
+  }
 }
